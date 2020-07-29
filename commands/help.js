480235b97ea41d1e6b5afa3775c43f2abafe832c
theya <ashorking.theyagamer@gmail.com>
2020-07-29 15:22:57 +0200

    proper help command

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,35 @@
+const Discord = require("discord.js");
+
 module.exports = {
-	name: 'help',
-	execute(message) {
-        message.channel.send(
-            "`'start (Number of songs) (OP/ED)` : Start a new quiz.\n`'next` : Skip a song.\n`'leave` : Stop the quiz.\n`'points` : Show points for the current quiz.\n`'set (Anilist Username)` : Set your anilist."
-          );
-	},
+  name: "help",
+  execute(message) {
+    const embed = new Discord.MessageEmbed()
+      .addFields(
+        {
+          name: "**'start** *(Number of songs) (OP/ED)*",
+          value: "Start a new quiz",
+        },
+        {
+          name: "**'next**",
+          value: "Skip the current theme.",
+        },
+        {
+          name: "**'leave**",
+          value: "Stop the quiz.",
+        },
+        {
+          name: "**'points**",
+          value: "Show the leaderboard for the current quiz.",
+        },
+        {
+          name: "**'set** *(Anilist username)*",
+          value: "Set your anilist profile.",
+        }
+      )
+      .setAuthor("Noel's commands list", "", "https://github.com/theyadev/noel")
+      .setDescription("Here's all the commands for the bot:")
+      .setTimestamp(new Date())
+      .setColor(1127128);
+    message.channel.send(embed);
+  },
 };
